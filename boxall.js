773ef78fae4b1f4b78c0d0a07bde935e82c11ade
wyliebreck <43286892+wyliebreck@users.noreply.github.com>
2022-03-22 07:09:01 +1100

    Update boxall.js

--- boxall.js
+++ boxall.js
@@ -1,18 +1,18 @@
 window.addEventListener("keydown", function(e){
 	if (e.ctrlKey && e.key === "b") {
-    if (document.boxesOn) {
-      document.querySelectorAll("*").forEach(function(x){
-        x.style.outline = x.oldOutlineStyle;
-        delete x.oldOutlineStyle;
-      });
-      delete document.boxesOn;
-    }
-    else {
-      document.querySelectorAll("*").forEach(function(x){
-        x.oldOutlineStyle = x.style.outline;
-        x.style.outline = "thin red solid";
-      });
-      document.boxesOn = true;
-    }
-  }
+		if (document.boxesOn) {
+			document.querySelectorAll("*").forEach(function(x){
+				x.style.outline = x.oldOutlineStyle;
+				delete x.oldOutlineStyle;
+			});
+			delete document.boxesOn;
+		}
+		else {
+			document.querySelectorAll("*").forEach(function(x){
+				x.oldOutlineStyle = x.style.outline;
+				x.style.outline = "thin red solid";
+			});
+			document.boxesOn = true;
+		}
+	}
 });
